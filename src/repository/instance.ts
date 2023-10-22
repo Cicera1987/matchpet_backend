1047273a321fc0cc0767ab27aebd4d27de0632f3
Cicera Ribeiro <maccicera@gmail.com>
2023-10-22 17:11:32 -0300

    atualizações de erros

--- src/repository/instance.ts
+++ src/repository/instance.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+    
+})
+
+
+
+
